571e0dbb3137670094f89acefb7b9d393507d13d
Nono Martinez Alonso <mundowarezweb@gmail.com>
2017-09-15 10:53:22 -0400

    First commit 1.0.0

---
+++ DynamoLoggie/DynamoLoggie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+using LoggieLib;
+
+namespace DynamoLoggie
+{
+    public class DynamoLoggie
+    {
+        private DynamoLoggie()
+        {
+            Loggie.Write("Hello, Loggie!");
+        }
+
+        [CanUpdatePeriodically(true)]
+        public static string GetLog()
+        {
+            return Loggie.GetString();
+        }
+
+        public static void Write(string String)
+        {
+            Loggie.Write(String);
+        }
+
+        public static void ClearLog(bool Run)
+        {
+            if (Run)
+            {
+                Loggie.Clear();
+            }
+        }
+    }
+}
---
+++ Loggie/Loggie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace LoggieLib
+{
+    public class Loggie
+    {
+        // StringBuilder to build the log
+        public static StringBuilder sb = new StringBuilder();
+
+        private Loggie()
+        {
+
+        }
+
+        // Preprend a new line to the log
+        public static void Write(string String)
+        {
+            sb.Insert(0, Environment.NewLine);
+            sb.Insert(0, String);
+        }
+
+        // Clear existing log
+        public static void Clear()
+        {
+            sb = new StringBuilder();
+        }
+
+        // Get a string representation of the current log
+        public static string GetString()
+        {
+            return sb.ToString();
+        }
+    }
+}
